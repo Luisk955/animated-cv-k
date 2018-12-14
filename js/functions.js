1180e850a2759cb2dea557cb253da07671e8abda
Luisk955 <ldelvalleu@ucenfotec.ac.cr>
2018-12-13 18:29:59 -0600

    mandese

--- js/functions.js
+++ js/functions.js
@@ -6,4 +6,19 @@ class Animator {
     animateUsingTweenMax(pElement, pOptions) {
         TweenMax.from(pElement, 4, pOptions);
     }
+
+    animateProgress(pElement) {
+        TweenMax.from(pElement, 2, {
+            css: {
+                rotationY: 360,
+                transformOrigin: "50% 50% -400px",
+                backgroundColor: "hsl(" + Math.random() * 255 + ", 90%, 60%)"
+            },
+            repeat: 1,
+            yoyo: true,
+            repeatDelay: 1,
+            ease: Linear.easeNone
+        });
+    }
+
 }
--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ document.addEventListener(`DOMContentLoaded`, (event) => {
 
     let buttonNext = document.getElementById(`buttonNext`),
         buttonPrevious = document.getElementById(`buttonPrevoius`),
-        sections = document.getElementsByClassName(`curriculumSection`);
-    console.log('sections: ', sections);
+        sections = document.getElementsByClassName(`curriculumSection`),
+        progressCircle = document.getElementsByClassName('progress-circle');
+    console.log('progressCircle: ', progressCircle);
 
 
     // let nameOptions = {
@@ -25,6 +26,56 @@ document.addEventListener(`DOMContentLoaded`, (event) => {
     buttonPrevious.addEventListener(`click`, () => {
         showNextPage(false);
     });
+
+    function showNextPage(pValue) {
+        if (pValue) {
+            for (let j = 0; j < sections.length; j++) {
+                if (sections[j].classList[1] == `show`) {
+                    sections[j].classList.remove(`show`)
+                    sections[j].classList.add(`hide`);
+                    let sectionNumber;
+                    if (j == (sections.length - 1)) {
+                        sections[0].classList.remove(`hide`);
+                        sections[0].classList.add(`show`);
+                    } else {
+                        sectionNumber = j + 1;
+                        sections[sectionNumber].classList.remove(`hide`);
+                        sections[sectionNumber].classList.add(`show`);
+                    }
+                    j = sections.length;
+                }
+            }
+        } else {
+            for (let j = 0; j < sections.length; j++) {
+                if (sections[j].classList[1] == `show`) {
+                    sections[j].classList.remove(`show`)
+                    sections[j].classList.add(`hide`);
+                    let sectionNumber;
+                    if (j == 0) {
+                        sections[(sections.length - 1)].classList.remove(`hide`);
+                        sections[(sections.length - 1)].classList.add(`show`);
+                    } else {
+                        sectionNumber = j - 1;
+                        sections[sectionNumber].classList.remove(`hide`);
+                        sections[sectionNumber].classList.add(`show`);
+                    }
+                    j = sections.length;
+                }
+
+            }
+        }
+    }
+
+
+
+    for (let i = 0; i < progress.length; i++) {
+        animator.animateProgress(progress[i]);
+
+    }
+
+
+
+    // -----------
 
 
     function update() {
@@ -152,50 +203,7 @@ document.addEventListener(`DOMContentLoaded`, (event) => {
 
     }
 
-    function showNextPage(pValue) {
-        if (pValue) {
-            for (let j = 0; j < sections.length; j++) {
-                if (sections[j].classList[1] == `show`) {
-                    sections[j].classList.remove(`show`)
-                    sections[j].classList.add(`hide`);
-                    let sectionNumber;
-                    if (j == (sections.length - 1)) {
-                        sections[0].classList.remove(`hide`);
-                        sections[0].classList.add(`show`);
-                    } else {
-                        sectionNumber = j + 1;
-                        sections[sectionNumber].classList.remove(`hide`);
-                        sections[sectionNumber].classList.add(`show`);
 
-                    }
-
-                    j = sections.length;
-                }
-
-            }
-        } else {
-            for (let j = 0; j < sections.length; j++) {
-                if (sections[j].classList[1] == `show`) {
-                    sections[j].classList.remove(`show`)
-                    sections[j].classList.add(`hide`);
-                    let sectionNumber;
-                    if (j == 0) {
-                        sections[(sections.length - 1)].classList.remove(`hide`);
-                        sections[(sections.length - 1)].classList.add(`show`);
-                    } else {
-                        sectionNumber = j - 1;
-                        sections[sectionNumber].classList.remove(`hide`);
-                        sections[sectionNumber].classList.add(`show`);
-
-                    }
-
-                    j = sections.length;
-                }
-
-            }
-        }
-
-    }
 
 
 
@@ -207,166 +215,3 @@ document.addEventListener(`DOMContentLoaded`, (event) => {
         pContainer.style.display = pDisplay;
     }
 });
-
-
-var commute = new Boolean;
-
-function css3RadialProgressBar() {
-
-    var xvaluenow;
-
-    console.log(commute);
-
-    if (commute == 0) {
-        xvaluenow = 80;
-        var progreso = document.getElementById('progreso');
-        progreso.innerHTML = 'Progress ' + xvaluenow + '%';
-        commute = 1;
-    } else {
-        xvaluenow = 0;
-
-        var progreso = document.getElementById('progreso');
-        progreso.innerHTML = 'Click to view';
-        commute = 0;
-
-    };
-
-
-    var rotatenum = 'rotate(' + xvaluenow * 1.8 + 'deg)';
-    //var progress = document.getElementById('progress');
-
-    var progress_circle = document.getElementById('progress-circle');
-    var progress_style = document.getElementById('progress-style');
-
-    /* Fix: Cover gap with shadow */
-    if (xvaluenow == 0) {
-        var shadowfix = "0";
-    } else {
-        var shadowfix = "1px";
-    }
-
-
-
-    progress_circle.setAttribute("aria-valuenow", xvaluenow);
-    progress_style.innerHTML = " \
-.p-h:before, .p-f, .p-f:before{ \
--moz-transform: " + rotatenum + "; \
--webkit-transform: " + rotatenum + "; \
--o-transform: " + rotatenum + "; \
--ms-transform: " + rotatenum + "; \
-transform: " + rotatenum + "; \
--webkit-box-shadow: 0 0 0 " + shadowfix + " #828282; \
-  box-shadow: 0 0 0 " + shadowfix + " #828282;}\
-\ ";
-}
-/*----------------------------------------------------*/
-
-var commute01 = new Boolean;
-
-function css3RadialProgressBar01() {
-
-    var xvaluenow01;
-
-    console.log(commute01);
-
-    if (commute01 == 0) {
-        xvaluenow01 = 70;
-        var progreso01 = document.getElementById('progreso01');
-        progreso01.innerHTML = 'Progress ' + xvaluenow01 + '%';
-        commute01 = 1;
-    } else {
-        xvaluenow01 = 0;
-
-        var progreso01 = document.getElementById('progreso01');
-        progreso01.innerHTML = 'Click to view';
-        commute01 = 0;
-
-    };
-
-
-    var rotatenum = 'rotate(' + xvaluenow01 * 1.8 + 'deg)';
-    //var progress = document.getElementById('progress');
-
-    var progress_circle01 = document.getElementById('progress-circle01');
-    var progress_style01 = document.getElementById('progress-style01');
-
-    /* Fix: Cover gap with shadow */
-    if (xvaluenow01 == 0) {
-        var shadowfix = "0";
-    } else {
-        var shadowfix = "1px";
-    }
-
-
-
-    progress_circle01.setAttribute("aria-valuenow", xvaluenow01);
-    progress_style01.innerHTML = " \
-.p-h01:before, .p-f01, .p-f01:before{ \
--moz-transform: " + rotatenum + "; \
--webkit-transform: " + rotatenum + "; \
--o-transform: " + rotatenum + "; \
--ms-transform: " + rotatenum + "; \
-transform: " + rotatenum + "; \
--webkit-box-shadow: 0 0 0 " + shadowfix + " #828282; \
-  box-shadow: 0 0 0 " + shadowfix + " #828282;}\
-\ ";
-}
-
-
-//document.body.onload = function() {css3RadialProgressBar()};
-
-/*----------------------------------------------------*/
-
-var commute02 = new Boolean;
-
-function css3RadialProgressBar02() {
-
-    var xvaluenow02;
-
-    console.log(commute02);
-
-    if (commute02 == 0) {
-        xvaluenow02 = 50;
-        var progreso02 = document.getElementById('progreso02');
-        progreso02.innerHTML = 'Progress ' + xvaluenow02 + '%';
-        commute02 = 1;
-    } else {
-        xvaluenow02 = 0;
-
-        var progreso02 = document.getElementById('progreso02');
-        progreso02.innerHTML = 'Click to view';
-        commute02 = 0;
-
-    };
-
-
-    var rotatenum = 'rotate(' + xvaluenow02 * 1.8 + 'deg)';
-    //var progress = document.getElementById('progress');
-
-    var progress_circle02 = document.getElementById('progress-circle02');
-    var progress_style02 = document.getElementById('progress-style02');
-
-    /* Fix: Cover gap with shadow */
-    if (xvaluenow02 == 0) {
-        var shadowfix = "0";
-    } else {
-        var shadowfix = "1px";
-    }
-
-
-
-    progress_circle02.setAttribute("aria-valuenow", xvaluenow02);
-    progress_style02.innerHTML = " \
-.p-h02:before, .p-f02, .p-f02:before{ \
--moz-transform: " + rotatenum + "; \
--webkit-transform: " + rotatenum + "; \
--o-transform: " + rotatenum + "; \
--ms-transform: " + rotatenum + "; \
-transform: " + rotatenum + "; \
--webkit-box-shadow: 0 0 0 " + shadowfix + " #828282; \
-  box-shadow: 0 0 0 " + shadowfix + " #828282;}\
-\ ";
-}
-
-
-//document.body.onload = function() {css3RadialProgressBar()};
